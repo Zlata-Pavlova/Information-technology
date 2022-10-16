be54a6eacf2f3d99efe969eb2d60b7df88bee674
Андрей Боев <a.boev.it@gmail.com>
2022-10-16 14:32:56 +0300

    16.10.2022 (4, 6)

---
+++ Lab 3/number 4/number 4/number 4.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define N 15
+
+int main()
+{
+	int a[N];
+	FILE* fin = fopen("input.txt", "r");
+	if (fin == NULL)
+	{
+		printf("Can't open file!\n");
+		return 1;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		fscanf(fin, "%d", &a[i]);
+		printf("%d ", a[i]);
+		if (a[i] > 0)
+			a[i] = -a[i];
+		else
+			a[i] = -a[i] % 10;
+	}
+	printf("\n");
+	for (int i = 0; i < N; i++)
+		printf("%d ", a[i]);
+
+	printf("\n");
+	return 0;
+}
---
+++ Lab 3/number 6/number 6/number 6.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+#define M 10
+#define A -10
+#define B 10
+
+
+int main()
+{
+	srand(time(0));
+	int sum = 0, sum2 = 0, min = B + 1;
+	int init_array[N][M];
+	int new_array[N][M];
+	FILE* fout = fopen("array.txt", "w");
+	FILE* fin;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			init_array[i][j] = A + (rand() % (B - A + 1));
+			fprintf(fout, "%-4d", init_array[i][j]);
+		}
+		fprintf(fout, "\n");
+	}
+	fclose(fout);
+	fin = fopen("array.txt", "r");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			fscanf(fout, "%d", &new_array[i][j]);
+			printf("%4d", new_array[i][j]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+		{
+			if (new_array[i][j] > 0)
+			{
+				sum += new_array[i][j];
+			}
+		}
+	for (int i = 0; i < M; i++)
+		sum2 += new_array[3][i];
+
+	printf("\nSum = %d\n", sum);
+	printf("Average = %f\n", (float)sum2 / M);
+	// Проверка на квадратный массив, т.к. у прочих нет главной диагонали.
+	if (N == M)
+	{
+		for (int i = 1; i < N; i++)
+		{
+			if (new_array[i][i] > 0 && new_array[i][i] < min)
+			{
+				min = new_array[i][i];
+			}
+		}
+		printf("Min = %d", min);
+	}
+	fclose(fin);
+	return 0;
+}
