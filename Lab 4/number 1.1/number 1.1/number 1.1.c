77970fb2c2bf54556b0e35970b478e5b75b8307a
Андрей Боев <a.boev.it@gmail.com>
2022-10-23 14:29:30 +0300

    has been added lab 4 numbers 1.1

---
+++ Lab 4/number 1.1/number 1.1/number 1.1.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+#define M 10
+#define A -10
+#define B 10
+
+int main()
+{
+	int matrix[N][M];
+	int* array;
+	int size = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			matrix[i][j] = A + (rand() % (B - A + 1));
+			printf("%-4d", matrix[i][j]);
+
+			if (i % 2 == 0 && j % 2 != 0 && matrix[i][j] < 0)
+				size++;
+		}
+		printf("\n");
+	}
+	printf("\n");
+	array = (int*)malloc(sizeof(int) * size);
+	int arrayIndex = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (i % 2 == 0 && j % 2 != 0 && matrix[i][j] < 0)
+			{
+				array[arrayIndex] = matrix[i][j];
+				printf("%-4d", array[arrayIndex]);
+			}
+
+	return 0;
+}
