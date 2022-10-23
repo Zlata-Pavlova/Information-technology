e4cc31854df779a0afa322e1bbe7ce47eae8c98e
Андрей Боев <a.boev.it@gmail.com>
2022-10-23 14:17:33 +0300

    has been added lab 4 number 1

---
+++ Lab 4/number 1/number 1/number 1.c
@@ -0,0 +1,32 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define N 10
+
+int main()
+{
+	int initArray[N];
+	int size = 0;
+	int* newArray;
+	srand(time(0));
+	for (int i = 0; i < N; i++)
+	{
+		initArray[i] = rand() % 100;
+		printf("%3d", initArray[i]);
+
+		if (initArray[i] % 4 == 0 && initArray[i] % 2 == 0)
+			size++;
+	}
+	printf("\n");
+	newArray = (int*)malloc(sizeof(int) * size);
+
+	for (int i = 0, j = 0; i < size; i++)
+		if (initArray[i] % 4 == 0 && initArray[i] % 2 == 0)
+		{
+			newArray[j] = initArray[i];
+			printf("%3d", newArray[j++]);
+		}
+
+
+	return 0;
+}
