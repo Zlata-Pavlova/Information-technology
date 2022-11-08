b38d477a3e80fa507dcfd68e040bb40be4bc88f5
Андрей Боев <a.boev.it@gmail.com>
2022-11-08 13:20:41 +0300

    Lab 2 number 9

---
+++ Lab 2/number 9/number 9/number 9.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#define SIZE 10
+
+int main()
+{
+	setlocale(0, "RU");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char names[SIZE][1024];
+	for (int i = 0; i < SIZE; i++)
+	{
+		scanf("%s", &names[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (strchr("бвгджзйклмнпрстфхцчшщ", names[i][strlen(names[i]) - 1]))
+			printf("%s\n", names[i]);
+	}
+
+	return 0;
+}
