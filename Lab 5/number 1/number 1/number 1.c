e73ad12baf41f390c7322070705ffb7358486985
Андрей Боев <a.boev.it@gmail.com>
2022-11-20 13:49:04 +0300

    Added Lab 5 number 1

---
+++ Lab 5/number 1/number 1/number 1.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	srand(time(NULL));
+	// Открытие файла на перезапись
+	FILE* out = fopen("output.txt", "w");
+	int n, m;
+	printf("N: ");
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		fprintf(out, "%d ", rand() % 100);
+	fclose(out);
+	// Открытие файла на дозапись
+	out = fopen("output.txt", "a");
+	printf("M: ");
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++)
+		fprintf(out, "%c ", rand() % 128);
+	fclose(out);
+	return 0;
+}
