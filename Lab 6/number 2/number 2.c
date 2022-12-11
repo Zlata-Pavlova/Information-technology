bf462b72431b736bb6c8d6263f8eee3053f7ef3a
Андрей Боев <a.boev.it@gmail.com>
2022-12-11 14:40:09 +0300

    Lab 6: 1, 2, 3

---
+++ Lab 6/number 2/number 2.c
@@ -0,0 +1,93 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define LENGTH 256
+
+struct OS
+{
+	char name[LENGTH];
+	char family[LENGTH];
+	int popularity;
+	int isFree;
+};
+
+typedef struct OS OperatingSystem;
+
+int LoadFile(char* FileName, OperatingSystem** os)
+{
+	FILE* in = fopen(FileName, "r");
+	char line[1024];
+	int size = 0;
+	char* ptr;
+	int i;
+	int index = 0;
+	// Считаем кол-во строк в файле
+	while (!feof(in))
+	{
+		fgets(line, 1024, in);
+		size++;
+	}
+	(*os) = (OperatingSystem*)malloc(sizeof(OperatingSystem) * size);
+	// Перемещаемся в начало файла
+	fseek(in, 0, SEEK_SET);
+	while (!feof(in))
+	{
+		fgets(line, 1024, in);
+		ptr = strtok(line, ",");
+		i = 0;
+		while (ptr != NULL)
+		{
+			switch (i)
+			{
+			case 0:
+				strcpy((*os)[index].name, ptr);
+				break;
+			case 1:
+				strcpy((*os)[index].family, ptr);
+				break;
+			case 2:
+				sscanf(ptr, "%d", &(*os)[index].popularity);
+				break;
+			case 3:
+				sscanf(ptr, "%d", &(*os)[index].isFree);
+				break;
+			}
+			i++;
+			ptr = strtok(NULL, ",");
+		}
+		index++;
+	}
+	return size;
+}
+
+int main()
+{
+	setlocale(0, "RU");
+	int size;
+	double avgPopularity = 0.0;
+	OperatingSystem* os;
+	size = LoadFile("operating_systems.txt", &os);
+	printf("Платные ОС:\n");
+	for (int i = 0; i < size; i++)
+	{
+		if (os[i].isFree)
+		{
+			printf("%-20s%-10s%-4d%s\n", os[i].name, os[i].family, os[i].popularity,
+				(os[i].isFree ? "Бесплатная" : "Платная"));
+		}
+		avgPopularity += os[i].popularity;
+	}
+	avgPopularity /= size;
+	printf("Популярность меньше средней:\n");
+	for (int i = 0; i < size; i++)
+	{
+		if (os[i].popularity < avgPopularity)
+		{
+			printf("%-20s%-10s%-4d%s\n", os[i].name, os[i].family, os[i].popularity,
+				(os[i].isFree ? "Бесплатная" : "Платная"));
+		}
+	}
+
+	return 0;
+}
---
+++ Lab 6/number 3/number 3.c
@@ -0,0 +1,83 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define LENGTH 256
+
+struct _Agriculture
+{
+	char name[LENGTH];
+	float cropVolume;
+	float sowingArea;
+};
+
+typedef struct _Agriculture Agriculture;
+
+struct _Agronomy
+{
+	char region[LENGTH];
+	int size;
+	Agriculture agricultures[10];
+};
+
+typedef struct _Agronomy Agronomy;
+
+int main()
+{
+	Agronomy agronomy[3] = { 0 };
+	FILE* in = fopen("agronomy.txt", "r");
+	int index = 0;
+	char line[1024];
+	char* ptr;
+	int i, contains;
+
+	while (!feof(in))
+	{
+		fgets(line, 1024, in);
+		ptr = strtok(line, ",");
+		i = 0;
+		while (ptr != NULL)
+		{
+			switch (i)
+			{
+			case 0:
+				index = 0;
+				contains = 0;
+				for (size_t r = 0; r < 3; r++)
+				{
+					if (strcmp(ptr, agronomy[r].region) == 0)
+					{
+						index = r;
+						contains = 1;
+						break;
+					}
+					if (strcmp(ptr, agronomy[r].region) != 0 && strlen(agronomy[r].region) == 0)
+					{
+						index++;
+					}
+				}
+				if (!contains)
+				{
+					strcpy(agronomy[index].region, ptr);
+					agronomy[index].size = 0;
+				}
+				break;
+			case 1:
+				strcpy(agronomy[index].agricultures[agronomy[index].size].name, ptr);
+				break;
+			case 2:
+				sscanf(ptr, "%f", &agronomy[index].agricultures[agronomy[index].size].cropVolume);
+				break;
+			case 3:
+				sscanf(ptr, "%f", &agronomy[index].agricultures[agronomy[index].size].sowingArea);
+				agronomy[index].size++;
+				break;
+			}
+			i++;
+			ptr = strtok(NULL, ",");
+		}
+	}
+
+
+	return 0;
+}
